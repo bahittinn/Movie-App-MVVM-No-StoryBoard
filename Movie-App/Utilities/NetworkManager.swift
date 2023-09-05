d378af92c628045d4800e3ec9ad2bc967f3f767d
bahittinn <bahittin84@gmail.com>
2023-09-05 22:24:02 +0300

    network service

--- Movie-App/Utilities/NetworkManager.swift
+++ Movie-App/Utilities/NetworkManager.swift
@@ -8,5 +8,24 @@
 import Foundation
 
 class NetworkManager {
+    static let shared = NetworkManager()
+    private init() {}
     
+    func download(url: URL, completion: @escaping (Result<Data, Error>) -> ()) {
+        URLSession.shared.dataTask(with: url) { data, response, error in
+            if let error = error {
+                print("DEBUG: error is \(error.localizedDescription)")
+                completion(.failure(error))
+                return
+            }
+            
+            guard let response = response as? HTTPURLResponse, response.statusCode == 200 else {
+                completion(.failure(URLError(.badServerResponse)))
+                return
+            }
+            
+            guard let data = data else { return }
+            
+        }
+    }
 }
