3ff7735ed318713cd26db804bb7225835fef6b89
bahittinn <bahittin84@gmail.com>
2023-09-05 21:58:46 +0300

    folder structure

---
+++ Movie-App/Core/HomeScreen/HomeScreen.swift
@@ -0,0 +1,29 @@
+//
+//  HomeScreen.swift
+//  Movie-App
+//
+//  Created by Bahittin on 5.09.2023.
+//
+
+import UIKit
+
+class HomeScreen: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ Movie-App/Utilities/NetworkManager.swift
@@ -0,0 +1,12 @@
+//
+//  NetworkManager.swift
+//  Movie-App
+//
+//  Created by Bahittin on 5.09.2023.
+//
+
+import Foundation
+
+class NetworkManager {
+    
+}
