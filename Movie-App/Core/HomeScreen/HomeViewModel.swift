27fedad5113dd520789ed840970c2564dad46d78
bahittinn <bahittin84@gmail.com>
2023-09-07 21:32:42 +0300

    HomeViewModel

---
+++ Movie-App/Core/HomeScreen/HomeViewModel.swift
@@ -0,0 +1,21 @@
+//
+//  HomeViewModel.swift
+//  Movie-App
+//
+//  Created by Bahittin on 7.09.2023.
+//
+
+import Foundation
+
+protocol HomeViewModelInterFace {
+    
+}
+
+
+final class HomeViewModel {
+    
+}
+
+extension HomeViewModel: HomeViewModelInterFace {
+    
+}
